0a5e77b6320b12714ac1127f4ac1b90f949cf9d1
방소현 <markupbang@gmail.com>
2017-05-12 18:01:03 +0900

    title bar style

--- main.js
+++ main.js
@@ -1,64 +1,28 @@
 const {app, BrowserWindow} = require('electron');
-const path = require('path');
-const url = require('url');
 
-let mainWindow;
+let first = null,
+    second = null,
+    third = null;
 
-function createWindow () {
-    // renderer process 생성
-    console.log('Create window');
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+app.on('ready', () => {
+    first = new BrowserWindow({
+        width: 600,
+        height: 600,
+        frame: false
+    });
+    first.loadURL(`file://${__dirname}/index.html`);
 
-    // renderer process 에 로딩할 컨텐츠 삽입
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+    second = new BrowserWindow({
+        width: 600,
+        height: 600,
+        titleBarStyle: 'hidden'
+    });
+    second.loadURL(`file://${__dirname}/hidden.html`);
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
-}
-
-/* main process 생성 완료 이벤트 바인딩 */
-app.on('ready', createWindow)
-
-/* 모든 윈도우가 닫히자 마자 불림 */
-app.on('window-all-closed', () => {
-    console.log('Window all close');
-
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
-
-/* 앱 종료가 시작되어서 앱의 모든 위도우들을 클로즈 하기 시작할때 불림. 이미 닫혀 있어도 불린다. */
-app.on('before-quit', (e) => {
-    console.log('before-quit');
-})
-
-// 모든 윈도우가 닫히고 나서 메인 앱 프로세스 종료하기 직전 불림
-app.on('will-quit',(e) => {
-    console.log('will-quit');
-})
-
-
-// 최종적으로 종료되면서 불림
-app.on('quit', (e, exitCode) => {
-    console.log(`quit : ${exitCode}`);
-})
-
-/*
-    [macOS]
-    dock 에서 어플리케이션 아이콘을 클릭하면 발생하는 이벤트
- */
-app.on('activate', (e, hasVisibleWindows) => {
-    /*
-        열려있는 윈도우가 있으면, hasVisibleWindows 가 true
-        없으면, hasVisibleWindows 가 false
-    */
-    console.log(`activate : ${hasVisibleWindows}`);
-    if (mainWindow === null) {
-        createWindow()
-    }
-})
+    third = new BrowserWindow({
+        width: 600,
+        height: 600,
+        titleBarStyle: 'hidden-inset'
+    });
+    third.loadURL(`file://${__dirname}/hidden-inset.html`);
+});
