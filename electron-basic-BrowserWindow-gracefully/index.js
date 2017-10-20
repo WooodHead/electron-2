4e8678bd8b9761c766af50606c629d5d4b84959c
markupbang <markupbang@gmail.com>
2017-10-20 10:13:25 +0900

    BrowserWindow garcefully 컨텐츠 준비가 되면 창을 띄우는 방식

---
+++ electron-basic-BrowserWindow-gracefully/index.js
@@ -0,0 +1,15 @@
+const {app, BrowserWindow} = require('electron');
+
+app.on('ready', () => {
+	const win = new BrowserWindow({
+		width: 600,
+		height: 600,
+		show: false
+	})
+
+	win.loadURL('https://github.com');
+
+	win.once('ready-to-show', () => {
+		win.show();
+	})
+})
